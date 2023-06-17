0cb445cd58e4a413a76d4571ea4a64924ef37206
water-mizuu <mizu.github.em@gmail.com>
2023-06-18 02:02:25 +0800

    Removed unnecessary `indices` extension method

--- lib/shared/extensions.dart
+++ lib/shared/extensions.dart
@@ -19,16 +19,12 @@ extension List2Extension<E> on List2<E> {
       yield <E>[for (int y = 0; y < this.length; ++y) this[y][x]];
     }
   }
-
-  Iterable<(int, int)> get indices sync* {
-    for (int y = 0; y < this.length; ++y) {
-      for (int x = 0; x < this[y].length; ++x) {
-        yield (y, x);
-      }
-    }
-  }
 }
 
 extension IterableListExtension<E> on Iterable<List<E>> {
-  Iterable<List<E>> get reversedRows => this.map((List<E> row) => row.reversed.toList());
+  Iterable<List<E>> get reversedRows sync* {
+    for (List<E> row in this) {
+      yield row.reversed.toList();
+    }
+  }
 }
