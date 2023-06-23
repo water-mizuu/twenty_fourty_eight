86fe89c75a1613fcb9a5cde0aca3de284aeb52eb
water-mizuu <mizu.github.em@gmail.com>
2023-06-23 17:06:33 +0800

    Changed `BoardBackground` to be more static

--- lib/widgets/board/board_background.dart
+++ lib/widgets/board/board_background.dart
@@ -1,6 +1,5 @@
 import "package:flutter/material.dart";
 import "package:provider/provider.dart";
-import "package:twenty_fourty_eight/data_structures/animated_tile.dart";
 import "package:twenty_fourty_eight/shared/constants.dart";
 import "package:twenty_fourty_eight/state/game_state.dart";
 import "package:twenty_fourty_eight/widgets/board/tile/background_tile.dart";
@@ -9,9 +8,14 @@ class BoardBackground extends StatelessWidget {
   const BoardBackground({super.key});
 
   @override
-  Widget build(BuildContext context) => Stack(
-        children: <Widget>[
-          for (var AnimatedTile(:int y, :int x) in context.select((GameState state) => state.flattenedGrid)) //
+  Widget build(BuildContext context) {
+    int gridY = context.select((GameState state) => state.gridY);
+    int gridX = context.select((GameState state) => state.gridX);
+
+    return Stack(
+      children: <Widget>[
+        for (int y = 0; y < gridY; ++y)
+          for (int x = 0; x < gridX; ++x)
             Positioned(
               top: y * Sizes.tileSize,
               left: x * Sizes.tileSize,
@@ -21,6 +25,7 @@ class BoardBackground extends StatelessWidget {
                 child: BackgroundTile(y: y, x: x),
               ),
             ),
-        ],
-      );
+      ],
+    );
+  }
 }
